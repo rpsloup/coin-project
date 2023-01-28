7baa5d71874ee227c4c58f28a354679d9801d483
rpsloup <rpsloup@proton.me>
2023-01-28 18:08:14 +0100

    FE: Creating the Default layout

---
+++ client/src/layouts/DefaultLayout.tsx
@@ -0,0 +1,12 @@
+interface IProps {
+  children: React.ReactNode;
+}
+
+const DefaultLayout = ({ children }: IProps): JSX.Element =>
+  (
+    <>
+      {children}
+    </>
+  );
+
+export default DefaultLayout;
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+import DefaultLayout from '../layouts/DefaultLayout';
+
 const HomePage = (): JSX.Element =>
   (
-    <>
+    <DefaultLayout>
       <h1>Home</h1>
-    </>
+    </DefaultLayout>
   );
 
 export default HomePage;
