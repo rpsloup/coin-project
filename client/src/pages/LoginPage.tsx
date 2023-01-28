62df155e4ca2aa4c1f23fb7d48df6f0c3fc98280
rpsloup <rpsloup@proton.me>
2023-01-28 19:08:47 +0100

    FE: Creating the Login page

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '../layouts/DefaultLayout';
+
+const LoginPage = (): JSX.Element =>
+  (
+    <DefaultLayout>
+      <h1>Login</h1>
+    </DefaultLayout>
+  );
+
+export default LoginPage;
