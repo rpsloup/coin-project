1a0f0d529b435b3f46c0d0d36786eb4e3e78f691
rpsloup <rpsloup@proton.me>
2023-01-28 19:02:13 +0100

    BE: Fetching the transactions from the PostgreSQL DB

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,21 +1,17 @@
 import { Router } from 'express';
 
-import type { Transaction } from '../../../typings/transactionTypes';
+import { pool } from '../main';
 
 const transactionRouter = Router();
 
-const exampleTransactionData: Transaction[] = [
-  { transaction_id: 0, sender: 'Jeff', receiver: 'Tim', amount: 15, timestamp: new Date().toJSON() },
-  { transaction_id: 1, sender: 'Jeff', receiver: 'Tim', amount: 8, timestamp: new Date().toJSON() },
-  { transaction_id: 2, sender: 'Tim', receiver: 'Jeff', amount: 7, timestamp: new Date().toJSON() },
-  { transaction_id: 3, sender: 'Jeff', receiver: 'Tim', amount: 32, timestamp: new Date().toJSON() },
-  { transaction_id: 4, sender: 'Tim', receiver: 'Jeff', amount: 6, timestamp: new Date().toJSON() },
-  { transaction_id: 5, sender: 'Tim', receiver: 'Jeff', amount: 71, timestamp: new Date().toJSON() },
-  { transaction_id: 6, sender: 'Jeff', receiver: 'Tim', amount: 25, timestamp: new Date().toJSON() },
-];
-
-transactionRouter.get('/', (_, res) => {
-  res.json(exampleTransactionData);
+transactionRouter.get('/', async (_, res) => {
+  try {
+    const transactions = await pool.query('SELECT * FROM Transactions');
+    res.json(transactions?.rows ?? []);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('Error!');
+  }
 });
 
 export default transactionRouter;
