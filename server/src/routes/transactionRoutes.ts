08911a882b9453da7f19d21274c2534bc2edc73f
rpsloup <rpsloup@proton.me>
2023-01-28 18:43:09 +0100

    BE: Sending example transaction data

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
 
+import type { Transaction } from '../../../typings/transactionTypes';
+
 const transactionRouter = Router();
 
+const exampleTransactionData: Transaction[] = [
+  { transaction_id: 0, sender: 'Jeff', receiver: 'Tim', amount: 15, date: new Date().toJSON() },
+  { transaction_id: 1, sender: 'Jeff', receiver: 'Tim', amount: 8, date: new Date().toJSON() },
+  { transaction_id: 2, sender: 'Tim', receiver: 'Jeff', amount: 7, date: new Date().toJSON() },
+  { transaction_id: 3, sender: 'Jeff', receiver: 'Tim', amount: 32, date: new Date().toJSON() },
+  { transaction_id: 4, sender: 'Tim', receiver: 'Jeff', amount: 6, date: new Date().toJSON() },
+  { transaction_id: 5, sender: 'Tim', receiver: 'Jeff', amount: 71, date: new Date().toJSON() },
+  { transaction_id: 6, sender: 'Jeff', receiver: 'Tim', amount: 25, date: new Date().toJSON() },
+];
+
 transactionRouter.get('/', (_, res) => {
-  res.json('Response!');
+  res.json(exampleTransactionData);
 });
 
 export default transactionRouter;
