88052362070f458cc72d50fcbcc73b157c803aba
rpsloup <rpsloup@proton.me>
2023-01-28 19:03:22 +0100

    BE: Fetching only one transaction by its ID

--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -14,4 +14,17 @@ transactionRouter.get('/', async (_, res) => {
   }
 });
 
+transactionRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const transactions = await pool.query('SELECT * FROM Transactions WHERE transaction_id = $1', [
+      id,
+    ]);
+    res.json(transactions?.rows[0] ?? null);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('Error!');
+  }
+});
+
 export default transactionRouter;
