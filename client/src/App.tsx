5996796af0d8658939a435d378344cb735c3f39e
rpsloup <rpsloup@proton.me>
2023-01-28 19:09:49 +0100

    FE: Creating the Register page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 
 const App = (): JSX.Element =>
   (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
